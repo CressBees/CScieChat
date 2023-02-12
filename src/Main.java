f1bb019ed6806999ebca040979866ecc9fd574e7
PickTheKiwi <pickj888@gmail.com>
2023-02-13 11:56:11 +1300

    Add imports

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+/*
+ * @authors: Seb White, Benji Cresswell
+ * @year: 2023
+ * This is the main class of the project.
+ */
+
+import java.net.*;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");

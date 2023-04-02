e90ce589bd124e903cafaa29595fbe3695cb810c
CressBe <CressBe@whs.school.nz>
2023-04-03 10:13:34 +1200

    Add Basic networking code

--- src/com/PTKaBC/Main.java
+++ src/com/PTKaBC/Main.java
@@ -5,8 +5,26 @@ package com.PTKaBC;
  * This is the main class of the project.
  */
 
+import java.io.*;
+import java.net.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Debug_StartUp");
+
+            //makes a listener on a port
+        try {
+            ServerSocket listenOn = new ServerSocket(26666);
+            Socket mainSocket = listenOn.accept();
+            DataInputStream readFromListenOn = new DataInputStream(mainSocket.getInputStream());
+            String message = readFromListenOn.readUTF();
+            System.out.println("Debug_ListenerCreated");
+            System.out.println("Message: " + message);
+            listenOn.close();
+        }
+        catch (Exception e) {
+            System.out.println("Debug_ListenerCreationError"+e);
+        }
+
     }
 }

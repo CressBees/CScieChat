82b515859b805687fecf7abc2ae7fd2ea5c22a21
PickTheKiwi <pickj888@gmail.com>
2023-05-25 14:56:34 +1200

    Add SYSTEM account

--- src/com/CScieChat/task/DataBase.java
+++ src/com/CScieChat/task/DataBase.java
@@ -2,10 +2,7 @@ package com.CScieChat.task;
 
 import com.CScieChat.Main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,20 +16,30 @@ public class DataBase {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:CScieChat.db");
             Statement statement = connection.createStatement(); // This creates a statement allowing us to execute SQL in the database
             /*
-             * Create two sql functions to create two tables. This is mostly self-explanatory, but I will explain the SQL written here
+             * Create three sql functions to create two tables. This is mostly self-explanatory, but I will explain the SQL written here
              * CREATE TABLE IF NOT EXISTS (table name) checks to see if a table exists in a database then creates one if it does not exist
              * Everything in the brackets is the data that's stored in each row, each one being a "column"
              * INTEGER and STRING are data types, similar to the same ones used in Java already
              * PRIMARY KEY refers to the primary key of a table, this can be used as a foreign key in other tables to refer to that table.
-             *
-             * This SQL can not be used by the SQL, so SQL injection can not be used here
+             * "SELECT * FROM userAccounts" selects everything from the userAccounts table,
+             * "WHERE username = 'SYSTEM'" filters that to only instances where the username column has "SYSTEM" as the username
              */
             String sql = "CREATE TABLE IF NOT EXISTS messages (unixTime INTEGER PRIMARY KEY, handler STRING)";
-            String sql2 = "CREATE TABLE IF NOT EXISTS users (username STRING, pass STRING)";
+            String sql2 = "CREATE TABLE IF NOT EXISTS userAccounts (username STRING PRIMARY KEY, pass STRING)";
+            String sql3 = "SELECT * FROM userAccounts WHERE username = 'SYSTEM'"; //
             // Execute the SQL and close the file
             // (would close the connection if it was connected to MySQL or something similar, however SQLite is file based)
             statement.executeUpdate(sql);
             statement.executeUpdate(sql2);
+            ResultSet result = statement.executeQuery(sql3);
+            if(result.getString("username") == null) {
+                String sql4 = "INSERT INTO userAccounts(username, pass) VALUES ('SYSTEM', '123')";
+                statement.executeUpdate(sql4);
+                System.out.println("Added SYSTEM account");
+            } else {
+                String user = result.getString("username");
+                System.out.println(user);
+            }
             statement.close();
             connection.close();
         } catch (SQLException e) {
